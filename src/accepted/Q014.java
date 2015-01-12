9a65a6ed3fb5cef48cf4ccbc872a60c5b865a7eb
ttyrueiwoqp <lufangjiannus@gmail.com>
2015-01-12 18:13:42 +0800

    Completed Q014

---
+++ src/accepted/Q014.java
@@ -0,0 +1,42 @@
+package accepted;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Q014 {
+	
+	public String longestCommonPrefix(String[] strs) {
+
+		if (strs == null || strs.length == 0) {
+			return "";
+		}
+		
+		if (strs.length == 1) {
+			return strs[0];
+		}
+		
+		StringBuilder res = new StringBuilder();
+		
+		for (int i = 0; i < strs[0].length(); i++) {
+			char curr = strs[0].charAt(i);
+			
+			for (int j = 1; j < strs.length; j++) {
+				if (i >= strs[j].length() || strs[j].charAt(i) != curr) {
+					return res.toString();
+				}
+			}
+			
+			res.append(curr);
+		}
+		
+		return res.toString();
+	}
+	
+	@Test
+	public void test() {
+		String[] a = {"abcd", "abcde", "abcdefg"};
+		assertEquals("abcd", longestCommonPrefix(a));
+	}
+	
+}
