ac7e9a701eccb8e0f19eb102781fca7c4e64af32
ttyrueiwoqp <lufangjiannus@gmail.com>
2015-01-26 17:00:28 +0800

    Completed Q172

---
+++ src/accepted/Q171_Q180/Q172.java
@@ -0,0 +1,41 @@
+package accepted.Q171_Q180;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Q172 {
+
+	public int trailingZeroes(int n) {
+
+		int sum = 0;
+		int k = 1;
+		int limit = Integer.MAX_VALUE / 5;
+		while (n >= k && k <= limit) {
+			k *= 5;
+			sum += n / k;
+		}
+		return sum;
+	}
+
+	public int trailingZeroes2(int n) {
+
+		int c = 0;
+		for (; n > 4; c += (n /= 5))
+			;
+		return c;
+	}
+
+	@Test
+	public void test() {
+		assertEquals(6, trailingZeroes(25));
+
+		System.out.println(1220703125);
+		System.out.println(Integer.MAX_VALUE / 5);
+
+		System.out.println(trailingZeroes(1808548325));
+		System.out.println(trailingZeroes2(1808548325));
+
+	}
+
+}
