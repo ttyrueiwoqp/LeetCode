0f01648c5b4b4603317c18def73bdec8ece77ebb
ttyrueiwoqp <lufangjiannus@gmail.com>
2015-01-27 19:54:53 +0800

    Completed Q067

---
+++ src/accepted/Q061_Q070/Q067.java
@@ -0,0 +1,47 @@
+package accepted.Q061_Q070;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Q067 {
+
+	public String addBinary(String a, String b) {
+
+		StringBuilder sb = new StringBuilder();
+		int aLen = a.length();
+		int bLen = b.length();
+		int carry = 0;
+
+		if (aLen > bLen) {
+			return addBinary(b, a);
+		}
+
+		for (int i = 1; i <= aLen; i++) {
+			int digit = a.charAt(aLen - i) - '0' + b.charAt(bLen - i) - '0' + carry;
+			int remain = digit % 2;
+			sb.insert(0, remain);
+			carry = digit / 2;
+		}
+		
+		for (int i = aLen + 1; i <= bLen; i++) {
+			int digit = b.charAt(bLen - i) - '0' + carry;
+			int remain = digit % 2;
+			sb.insert(0, remain);
+			carry = digit / 2;
+		}
+		
+		if (carry == 1) {
+			sb.insert(0, "1");
+		}
+
+		return sb.toString();
+	}
+
+	@Test
+	public void test() {
+		assertEquals("100", addBinary("1", "11"));
+		assertEquals("110110", addBinary("100", "110010"));
+	}
+
+}
--- src/qns/Q085.java
+++ src/qns/Q085.java
@@ -2,6 +2,9 @@ package qns;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class Q085 {
@@ -9,6 +12,9 @@ public class Q085 {
 	public int maximalRectangle(char[][] matrix) {
 
 		for (int i = 0; i < matrix.length; i++) {
+			
+			List<Integer> st = new ArrayList<Integer>();
+			
 			for (int j = 0; j < matrix[0].length; j++) {
 				
 			}
