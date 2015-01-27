ce9fa91f24bc9874a80944c20713852d4e3df4a3
ttyrueiwoqp <lufangjiannus@gmail.com>
2015-01-27 14:02:51 +0800

    Completed Q104

---
+++ src/accepted/Q101_Q110/Q104.java
@@ -0,0 +1,24 @@
+package accepted.Q101_Q110;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import util.TreeNode;
+
+public class Q104 {
+	
+	public int maxDepth(TreeNode root) {
+		
+		if (root == null) {
+			return 0;
+		}
+		return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+
+	}
+
+	@Test
+	public void test() {
+	}
+
+}
