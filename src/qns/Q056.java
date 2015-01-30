2571821902030dcdf78f910af62f8011a9f56e8d
ttyrueiwoqp <lufangjiannus@gmail.com>
2015-01-30 12:30:23 +0800

    Completed Q152

---
+++ src/qns/Q056.java
@@ -0,0 +1,24 @@
+package qns;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import util.Interval;
+
+public class Q056 {
+
+	public List<Interval> merge(List<Interval> intervals) {
+		
+		
+		return intervals;
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/qns/Q072.java
@@ -0,0 +1,21 @@
+package qns;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Q072 {
+
+	public int minDistance(String word1, String word2) {
+		
+		
+		
+		return 0;
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
