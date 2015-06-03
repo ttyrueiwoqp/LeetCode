36bc0a8f65b5cd1a1d391c1e4d8761cced6f7cb7
ttyrueiwoqp <lufangjiannus@gmail.com>
2015-06-03 18:29:18 +0800

    Completed Q080

--- src/accepted/Q021_Q030/Q026.java
+++ src/accepted/Q021_Q030/Q026.java
@@ -4,6 +4,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Given a sorted array, remove the duplicates in place
+ * such that each element appear only once and return the new length.
+ * <p>
+ * Do not allocate extra space for another array,
+ * you must do this in place with constant memory.
+ * <p>
+ * For example,
+ * Given input array nums = [1,1,2],
+ * <p>
+ * Your function should return length = 2,
+ * with the first two elements of nums being 1 and 2 respectively.
+ * It doesn't matter what you leave beyond the new length.
+ */
 public class Q026 {
 
 	public int removeDuplicates(int[] A) {
@@ -11,7 +25,7 @@ public class Q026 {
 		if (A.length < 2) {
 			return A.length;
 		}
-		
+
 		int p = 1;
 		for (int i = 1; i < A.length; i++) {
 			if (A[i] != A[i - 1]) {
@@ -25,7 +39,7 @@ public class Q026 {
 
 	@Test
 	public void test() {
-		int[] a = { 1, 1, 2, 2, 3, 4, 4, 5 };
+		int[] a = {1, 1, 2, 2, 3, 4, 4, 5};
 		assertEquals(5, removeDuplicates(a));
 	}
 
