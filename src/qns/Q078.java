cea0569c4cb7177505a1f817e1defb42f7e55648
ttyrueiwoqp <lufangjiannus@gmail.com>
2015-06-05 11:54:04 +0800

    Completed Q199

---
+++ src/qns/Q078.java
@@ -0,0 +1,31 @@
+package qns;
+
+import java.util.List;
+
+/**
+ * Created by LU-PC on 6/4/2015.
+ * <p>
+ * Given a set of distinct integers, nums, return all possible subsets.
+ * <p>
+ * Note:
+ * Elements in a subset must be in non-descending order.
+ * The solution set must not contain duplicate subsets.
+ * For example,
+ * If nums = [1,2,3], a solution is:
+ * <p>
+ * [
+ * [3],
+ * [1],
+ * [2],
+ * [1,2,3],
+ * [1,3],
+ * [2,3],
+ * [1,2],
+ * []
+ * ]
+ */
+public class Q078 {
+	public List<List<Integer>> subsets(int[] nums) {
+		return null;
+	}
+}
