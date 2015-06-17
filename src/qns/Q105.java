d2e32b23c0929c9e07756770905ee7189ab844ba
ttyrueiwoqp <lufangjiannus@gmail.com>
2015-06-17 19:38:47 +0800

    Completed Q033

---
+++ src/qns/Q105.java
@@ -0,0 +1,17 @@
+package qns;
+
+import util.TreeNode;
+
+/**
+ * Created by LU-PC on 6/17/2015.
+ * <p>
+ * Given preorder and inorder traversal of a tree, construct the binary tree.
+ * <p>
+ * Note:
+ * You may assume that duplicates do not exist in the tree.
+ */
+public class Q105 {
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		return null;
+	}
+}
---
+++ src/qns/Q108.java
@@ -0,0 +1,14 @@
+package qns;
+
+import util.TreeNode;
+
+/**
+ * Created by LU-PC on 6/17/2015.
+ * <p>
+ * Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+ */
+public class Q108 {
+	public TreeNode sortedArrayToBST(int[] nums) {
+		return null;
+	}
+}
