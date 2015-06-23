faae040136424589f41c4cef216158ba5993347e
Fangjian Lu <lufangjiannus@gmail.com>
2015-06-23 23:51:03 +0800

    Added Q224

---
+++ src/qns/Q224.java
@@ -0,0 +1,23 @@
+package qns;
+
+/**
+ * Created by FJ on 6/23/2015.
+ * <p/>
+ * Implement a basic calculator to evaluate a simple expression string.
+ * <p/>
+ * The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+ * <p/>
+ * You may assume that the given expression is always valid.
+ * <p/>
+ * Some examples:
+ * "1 + 1" = 2
+ * " 2-1 + 2 " = 3
+ * "(1+(4+5+2)-3)+(6+8)" = 23
+ * Note: Do not use the eval built-in library function.
+ */
+public class Q224 {
+	public int calculate(String s) {
+		return 0;
+
+	}
+}
