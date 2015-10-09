8ec4a0a31e433522067fdc9afd034371c75f3a17
ttyrueiwoqp <lufangjiannus@gmail.com>
2015-10-09 16:07:45 +0900

    Completed Q237

---
+++ src/accepted/Q231_Q240/Q237.java
@@ -0,0 +1,20 @@
+package accepted.Q231_Q240;
+
+import util.ListNode;
+
+/**
+ * Created by LU-PC on 10/9/2015.
+ * <p>
+ * Write a function to delete a node (except the tail) in a singly linked list,
+ * given only access to that node.
+ * <p>
+ * Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3,
+ * the linked list should become 1 -> 2 -> 4 after calling your function.
+ */
+public class Q237 {
+
+	public void deleteNode(ListNode node) {
+		node.val = node.next.val;
+		node.next = node.next.next;
+	}
+}
