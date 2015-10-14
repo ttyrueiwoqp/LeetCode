b3606855a9b1635f2fb3506d0cbeb17420ac5d9a
ttyrueiwoqp <lufangjiannus@gmail.com>
2015-10-14 20:16:43 +0900

    Added Q279

---
+++ src/qns/Q279.java
@@ -0,0 +1,19 @@
+package qns;
+
+/**
+ * Created by LU-PC on 10/14/2015.
+ * <p>
+ * Given a positive integer n, find the least number of
+ * perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+ * <p>
+ * For example, given n = 12, return 3 because 12 = 4 + 4 + 4;
+ * given n = 13, return 2 because 13 = 4 + 9.
+ */
+public class Q279 {
+	public int numSquares(int n) {
+
+
+
+		return 0;
+	}
+}
