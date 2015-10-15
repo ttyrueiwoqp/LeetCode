0d9e03eb504152014e23011d9eda13b57e3a7c5d
ttyrueiwoqp <lufangjiannus@gmail.com>
2015-10-15 20:44:58 +0900

    Added Q279

--- src/qns/Q279.java
+++ src/qns/Q279.java
@@ -1,5 +1,12 @@
 package qns;
 
+import org.junit.Test;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by LU-PC on 10/14/2015.
  * <p>
@@ -11,9 +18,35 @@ package qns;
  */
 public class Q279 {
 	public int numSquares(int n) {
+		int res = 0;
+		while (n > 0) {
+			n -= closetSquare(n);
+			res++;
+		}
+		return res;
+	}
 
+	private int closetSquare(int n) {
+		int sqrt = sqrt(n);
+		return sqrt * sqrt;
+	}
 
+	private int sqrt(int x) {
+		int g1 = x;
+		int g2 = iter(g1, x);
+		while (x / g1 < g1) {
+			g1 = g2;
+			g2 = iter(g2, x);
+		}
+		return g1;
+	}
 
-		return 0;
+	private int iter(int g, int x) {
+		return (g + x / g) / 2;
+	}
+
+	@Test
+	public void test() {
+
 	}
 }
