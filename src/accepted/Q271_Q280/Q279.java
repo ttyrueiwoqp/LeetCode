73f4e563524a485aef88dbf655fe9bac87bb0693
Fangjian Lu <lufangjiannus@gmail.com>
2015-10-16 21:01:20 +0800

    Completed Q279

--- src/accepted/Q271_Q280/Q279.java
+++ src/accepted/Q271_Q280/Q279.java
@@ -1,52 +1,39 @@
-package qns;
+package accepted.Q271_Q280;
 
 import org.junit.Test;
 
 import java.sql.Time;
 import java.sql.Timestamp;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 
 /**
  * Created by LU-PC on 10/14/2015.
- * <p>
+ * <p/>
  * Given a positive integer n, find the least number of
  * perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
- * <p>
+ * <p/>
  * For example, given n = 12, return 3 because 12 = 4 + 4 + 4;
  * given n = 13, return 2 because 13 = 4 + 9.
  */
 public class Q279 {
 	public int numSquares(int n) {
-		int res = 0;
-		while (n > 0) {
-			n -= closetSquare(n);
-			res++;
+		int[] dp = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			int min = Integer.MAX_VALUE;
+			int j = 1;
+			while(i - j*j >= 0) {
+				min = Math.min(min, dp[i - j*j] + 1);
+				j++;
+			}
+			dp[i] = min;
 		}
-		return res;
-	}
-
-	private int closetSquare(int n) {
-		int sqrt = sqrt(n);
-		return sqrt * sqrt;
-	}
-
-	private int sqrt(int x) {
-		int g1 = x;
-		int g2 = iter(g1, x);
-		while (x / g1 < g1) {
-			g1 = g2;
-			g2 = iter(g2, x);
-		}
-		return g1;
-	}
-
-	private int iter(int g, int x) {
-		return (g + x / g) / 2;
+		return dp[n];
 	}
 
 	@Test
 	public void test() {
-
+		System.out.println(numSquares(9975));
 	}
 }
