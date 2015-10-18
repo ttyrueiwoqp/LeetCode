ab874edbc4239fe2c39952a4b6cad58e81e0b2bd
Fangjian Lu <lufangjiannus@gmail.com>
2015-10-18 23:22:09 +0800

    Completed Q141

---
+++ src/accepted/Q141_Q150/Q141.java
@@ -0,0 +1,32 @@
+package accepted.Q141_Q150;
+
+import util.ListNode;
+
+/**
+ * Created by FJ on 10/18/2015.
+ * <p/>
+ * Given a linked list, determine if it has a cycle in it.
+ * <p/>
+ * Follow up:
+ * Can you solve it without using extra space?
+ */
+public class Q141 {
+
+	public boolean hasCycle(ListNode head) {
+		if (head == null) {
+			return false;
+		}
+
+		ListNode slow = head;
+		ListNode fast = head.next;
+		while (fast != null && fast.next != null) {
+			if (slow == fast || slow == fast.next) {
+				return true;
+			}
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+
+		return false;
+	}
+}
