9f78b2160be3c087b70776766421896f3d1981b5
ttyrueiwoqp <lufangjiannus@gmail.com>
2015-10-20 20:25:56 +0900

    Added Q238

---
+++ src/qns/Q238.java
@@ -0,0 +1,22 @@
+package qns;
+
+/**
+ * Created by LU-PC on 10/20/2015.
+ * <p>
+ * Given an array of n integers where n > 1, nums,
+ * return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+ * <p>
+ * Solve it without division and in O(n).
+ * <p>
+ * For example, given [1,2,3,4], return [24,12,8,6].
+ * <p>
+ * Follow up:
+ * Could you solve it with constant space complexity?
+ * (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+ */
+public class Q238 {
+	public int[] productExceptSelf(int[] nums) {
+
+		return null;
+	}
+}
