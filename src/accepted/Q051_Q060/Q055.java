466e1d02483c95ef44883e4e117ad8568f4d0cf4
Fangjian Lu <lufangjiannus@gmail.com>
2016-02-14 16:32:49 +0800

    Completed Q045

--- src/accepted/Q051_Q060/Q055.java
+++ src/accepted/Q051_Q060/Q055.java
@@ -4,6 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+ * <p/>
+ * Each element in the array represents your maximum jump length at that position.
+ * <p/>
+ * Determine if you are able to reach the last index.
+ * <p/>
+ * For example:
+ * A = [2,3,1,1,4], return true.
+ * <p/>
+ * A = [3,2,1,0,4], return false.
+ */
 public class Q055 {
 
 	public boolean canJump1(int[] A) {
@@ -39,10 +51,10 @@ public class Q055 {
 
 	@Test
 	public void test() {
-		int[] a = { 2, 3, 1, 1, 4 };
+		int[] a = {2, 3, 1, 1, 4};
 		assertTrue(canJump(a));
 
-		int[] b = { 3, 2, 1, 0, 4 };
+		int[] b = {3, 2, 1, 0, 4};
 		assertFalse(canJump(b));
 	}
 
