938335d0a555243487dd494efe576cf36d52cc63
Fangjian Lu <lufangjiannus@gmail.com>
2016-03-10 19:28:55 +0800

    Improved complexity of Q099

---
+++ src/accepted/Q091_Q100/Q099.java
@@ -0,0 +1,63 @@
+package accepted.Q091_Q100;
+
+import util.TreeNode;
+
+/**
+ * Created by FJ on 3/9/2016.
+ * <p/>
+ * Two elements of a binary search tree (BST) are swapped by mistake.
+ * <p/>
+ * Recover the tree without changing its structure.
+ * <p/>
+ * Note:
+ * A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+ * <p/>
+ * confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+ * <p/>
+ * OJ's Binary Tree Serialization:
+ * The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator where no node exists below.
+ * <p/>
+ * Here's an example:
+ * 1
+ * / \
+ * 2   3
+ * /
+ * 4
+ * \
+ * 5
+ * The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
+ */
+public class Q099 {
+
+	private TreeNode first = null;
+	private TreeNode second = null;
+	private TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+
+	public void recoverTree(TreeNode root) {
+		traverse(root);
+		swap(first, second);
+	}
+
+	private void traverse(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+
+		traverse(root.left);
+		if (root.val <= prev.val) {
+			if (first == null) {
+				first = prev;
+			}
+			second = root;
+		}
+
+		prev = root;
+		traverse(root.right);
+	}
+
+	private void swap(TreeNode first, TreeNode second) {
+		int temp = first.val;
+		first.val = second.val;
+		second.val = temp;
+	}
+}
---
+++ src/qns/Q327.java
@@ -0,0 +1,23 @@
+package qns;
+
+/**
+ * Created by FJ on 3/3/2016.
+ * <p/>
+ * Given an integer array nums, return the number of range sums that lie in [lower, upper] inclusive.
+ * Range sum S(i, j) is defined as the sum of the elements in nums between indices i and j (i ≤ j), inclusive.
+ * <p/>
+ * Note:
+ * A naive algorithm of O(n2) is trivial. You MUST do better than that.
+ * <p/>
+ * Example:
+ * Given nums = [-2, 5, -1], lower = -2, upper = 2,
+ * Return 3.
+ * The three ranges are : [0, 0], [2, 2], [0, 2] and their respective sums are: -2, -1, 2.
+ */
+public class Q327 {
+
+	public int countRangeSum(int[] nums, int lower, int upper) {
+
+		return 0;
+	}
+}
