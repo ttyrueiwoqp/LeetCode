7cb8f7acd9f6e1a9c835fcb508c4b8ffeb43d741
Fangjian Lu <lufangjiannus@gmail.com>
2016-03-10 22:48:12 +0800

    Completed Q063

--- src/accepted/Q061_Q070/Q063.java
+++ src/accepted/Q061_Q070/Q063.java
@@ -1,4 +1,4 @@
-package qns;
+package accepted.Q061_Q070;
 
 import org.junit.Test;
 
@@ -9,56 +9,56 @@ import static org.junit.Assert.assertEquals;
 
 /**
  * Created by LU-PC on 6/3/2015.
- * <p>
+ * <p/>
  * Follow up for "Unique Paths":
- * <p>
+ * <p/>
  * Now consider if some obstacles are added to the grids.
  * How many unique paths would there be?
- * <p>
+ * <p/>
  * An obstacle and empty space is marked as 1 and 0 respectively in the grid.
- * <p>
+ * <p/>
  * For example,
  * There is one obstacle in the middle of a 3x3 grid as illustrated below.
- * <p>
+ * <p/>
  * [
  * [0,0,0],
  * [0,1,0],
  * [0,0,0]
  * ]
  * The total number of unique paths is 2.
- * <p>
+ * <p/>
  * Note: m and n will be at most 100.
  */
 public class Q063 {
 	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-		return 0;
-	}
 
-	public int uniquePaths(int m, int n) {
+		int m = obstacleGrid.length;
+		if (m == 0) {
+			return 0;
+		}
+		int n = obstacleGrid[0].length;
 
-		return getRow(m+n-2).get(n-1);
-	}
-
-	public List<Integer> getRow(int rowIndex) {
-
-		List<Integer> res = new ArrayList<>();
-
-		if (rowIndex == 0) {
-			res.add(1);
-		} else {
-			List<Integer> last = getRow(rowIndex - 1);
-			for (int i = 0; i < last.size() - 1; i++) {
-				res.add(last.get(i) + last.get(i + 1));
+		int[][] dp = new int[m][n];
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (obstacleGrid[i][j] == 1) {
+					dp[i][j] = 0;
+				} else if (i == 0 && j == 0) {
+					dp[i][j] = 1;
+				} else {
+					int top = (i == 0) ? 0 : dp[i - 1][j];
+					int left = (j == 0) ? 0 : dp[i][j - 1];
+					dp[i][j] = top + left;
+				}
 			}
-			res.add(0, 1);
-			res.add(1);
 		}
 
-		return res;
+		return dp[m - 1][n - 1];
 	}
 
 	@Test
 	public void test() {
-		assertEquals(6, uniquePaths(3, 3));
+		int[][] a = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+		assertEquals(2, uniquePathsWithObstacles(a));
 	}
 }
