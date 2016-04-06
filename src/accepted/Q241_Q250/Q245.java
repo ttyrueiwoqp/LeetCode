a599f0c77fb88f9d278624964f63d77fd33d71c1
ttyrueiwoqp <lufangjiannus@gmail.com>
2016-04-06 21:08:11 +0800

    Completed Q245.

--- src/accepted/Q241_Q250/Q245.java
+++ src/accepted/Q241_Q250/Q245.java
@@ -46,25 +46,9 @@ public class Q245 {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public int sln1(String[] words, String word1, String word2) {
         long dist = Integer.MAX_VALUE, i1 = dist, i2 = -dist;
-        for (int i=0; i<words.length; i++) {
+        for (int i = 0; i < words.length; i++) {
             if (words[i].equals(word1))
                 i1 = i;
             if (words[i].equals(word2)) {
@@ -80,7 +64,7 @@ public class Q245 {
     public int sln2(String[] words, String word1, String word2) {
         long dist = Integer.MAX_VALUE, i1 = dist, i2 = -dist;
         boolean same = word1.equals(word2);
-        for (int i=0; i<words.length; i++) {
+        for (int i = 0; i < words.length; i++) {
             if (words[i].equals(word1)) {
                 if (same) {
                     i1 = i2;
