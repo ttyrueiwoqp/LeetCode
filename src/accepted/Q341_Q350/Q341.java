00fb996d170c8c232c3a898081c0f3645d4b90c2
ttyrueiwoqp <lufangjiannus@gmail.com>
2016-04-09 00:41:11 +0800

    Completed Q341.

---
+++ src/accepted/Q341_Q350/Q341.java
@@ -0,0 +1,64 @@
+package accepted.Q341_Q350;
+
+import java.util.*;
+
+/**
+ * Created by lvfan on 4/8/2016.
+ */
+public class Q341 {
+
+}
+// This is the interface that allows for creating nested lists.
+// You should not implement it, or speculate about its implementation
+interface NestedInteger {
+    // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    public boolean isInteger();
+
+    // @return the single integer that this NestedInteger holds, if it holds a single integer
+    // Return null if this NestedInteger holds a nested list
+    public Integer getInteger();
+
+    // @return the nested list that this NestedInteger holds, if it holds a nested list
+    // Return null if this NestedInteger holds a single integer
+    public List<NestedInteger> getList();
+}
+
+class NestedIterator implements Iterator<Integer> {
+
+    private Deque<ListIterator<NestedInteger>> stack = new ArrayDeque<>();
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        if (nestedList != null) {
+            stack.push(nestedList.listIterator());
+        }
+    }
+
+    @Override
+    public Integer next() {
+        hasNext();
+        return stack.peek().next().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty()) {
+            if (!stack.peek().hasNext()) {
+                stack.pop();
+            } else {
+                NestedInteger t = stack.peek().next();
+                if (t.isInteger()) {
+                    stack.peek().previous();
+                    return true;
+                }
+                stack.push(t.getList().listIterator());
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
