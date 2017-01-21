c5cfcad252aabbfba57b358097dbcd631f1cfe8a
ttyrueiwoqp <lufangjiannus@gmail.com>
2017-01-21 14:23:35 +0800

    Completed Q476

---
+++ src/accepted/Q471_Q480/Q476.java
@@ -0,0 +1,26 @@
+package accepted.Q471_Q480;
+
+/**
+ * Given a positive integer, output its complement number.
+ * The complement strategy is to flip the bits of its binary representation.
+ * <p>
+ * Note:
+ * The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+ * You could assume no leading zero bit in the integer’s binary representation.
+ * Example 1:
+ * Input: 5
+ * Output: 2
+ * Explanation: The binary representation of 5 is 101 (no leading zero bits),
+ * and its complement is 010. So you need to output 2.
+ * Example 2:
+ * Input: 1
+ * Output: 0
+ * Explanation: The binary representation of 1 is 1 (no leading zero bits),
+ * and its complement is 0. So you need to output 0.
+ */
+public class Q476 {
+
+    public int findComplement(int num) {
+        return Integer.highestOneBit(num) * 2 - 1 - num;
+    }
+}
