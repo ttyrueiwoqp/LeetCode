cec52c9afc7bf82482f7147b13fcc10bb62d5e8d
ttyrueiwoqp <lufangjiannus@gmail.com>
2017-04-30 16:38:21 +0800

    Completed Q507

---
+++ src/accepted/Q501_Q510/Q507.java
@@ -0,0 +1,29 @@
+package accepted.Q501_Q510;
+
+/**
+ * We define the Perfect Number is a positive integer that is equal to the sum of all its positive divisors except itself.
+ * <p>
+ * Now, given an integer n, write a function that returns true when it is a perfect number and false when it is not.
+ * Example:
+ * Input: 28
+ * Output: True
+ * Explanation: 28 = 1 + 2 + 4 + 7 + 14
+ * Note: The input number n will not exceed 100,000,000. (1e8)
+ */
+public class Q507 {
+
+    public boolean checkPerfectNumber(int num) {
+        if (num == 1) {
+            return false;
+        }
+
+        int sum = 1;
+        double sqrt = Math.sqrt(num);
+        for (int i = 2; i < sqrt; i++) {
+            if (num % i == 0) {
+                sum += i + num / i;
+            }
+        }
+        return sum == num;
+    }
+}
