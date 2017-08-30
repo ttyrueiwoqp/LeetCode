48866c16b891b40ac43622129ceb9fe69ba3a6c8
Lu Fangjian <lufangjiannus@gmail.com>
2017-08-30 15:54:05 +0800

    Completed Q636

--- src/accepted/Q631_Q640/Q636.java
+++ src/accepted/Q631_Q640/Q636.java
@@ -5,13 +5,17 @@ import java.util.Deque;
 import java.util.List;
 
 /**
- * Given the running logs of n functions that are executed in a nonpreemptive single threaded CPU, find the exclusive time of these functions.
+ * Given the running logs of n functions that are executed in a nonpreemptive single threaded CPU, find the exclusive
+ * time of these functions.
  * <p>
  * Each function has a unique id, start from 0 to n-1. A function may be called recursively or by another function.
  * <p>
- * A log is a string has this format : function_id:start_or_end:timestamp. For example, "0:start:0" means function 0 starts from the very beginning of time 0. "0:end:0" means function 0 ends to the very end of time 0.
+ * A log is a string has this format : function_id:start_or_end:timestamp. For example, "0:start:0" means function 0
+ * starts from the very beginning of time 0. "0:end:0" means function 0 ends to the very end of time 0.
  * <p>
- * Exclusive time of a function is defined as the time spent within this function, the time spent by calling other functions should not be considered as this function's exclusive time. You should return the exclusive time of each function sorted by their function id.
+ * Exclusive time of a function is defined as the time spent within this function, the time spent by calling other
+ * functions should not be considered as this function's exclusive time. You should return the exclusive time of each
+ * function sorted by their function id.
  * <p>
  * Example 1:
  * Input:
@@ -29,7 +33,8 @@ import java.util.List;
  * So function 0 totally execute 2 + 1 = 3 units of time, and function 1 totally execute 4 units of time.
  * Note:
  * Input logs will be sorted by timestamp, NOT log id.
- * Your output should be sorted by function id, which means the 0th element of your output corresponds to the exclusive time of function 0.
+ * Your output should be sorted by function id, which means the 0th element of your output corresponds to the exclusive
+ * time of function 0.
  * Two functions won't start or end at the same time.
  * Functions could be called recursively, and will always end.
  * 1 <= n <= 100
