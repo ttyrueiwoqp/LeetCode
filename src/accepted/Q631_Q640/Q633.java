479fd17fa22ba4e4ca24e5fc3f9389610cc58da6
Lu Fangjian <lufangjiannus@gmail.com>
2017-08-30 15:59:40 +0800

    Completed Q633

---
+++ src/accepted/Q631_Q640/Q633.java
@@ -0,0 +1,29 @@
+package accepted.Q631_Q640;
+
+/**
+ * Given a non-negative integer c, your task is to decide whether there're two integers a and b such that a2 + b2 = c.
+ * <p>
+ * Example 1:
+ * Input: 5
+ * Output: True
+ * Explanation: 1 * 1 + 2 * 2 = 5
+ * Example 2:
+ * Input: 3
+ * Output: False
+ */
+public class Q633 {
+    public boolean judgeSquareSum(int c) {
+        int i = 0, j = (int) Math.sqrt(c);
+        while (i <= j) {
+            int sum = i * i + j * j;
+            if (sum == c) {
+                return true;
+            } else if (sum < c) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+}
