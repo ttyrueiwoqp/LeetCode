25cc5b5c0db5c42b6b34669bac49c857ef83e65f
Lu Fangjian <lufangjiannus@gmail.com>
2017-08-30 15:47:00 +0800

    Completed Q131

--- src/accepted/Q131_Q140/Q131.java
+++ src/accepted/Q131_Q140/Q131.java
@@ -4,100 +4,102 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by FJ on 11/8/2015.
- * <p/>
  * Given a string s, partition s such that every substring of the partition is a palindrome.
- * <p/>
+ * <p>
  * Return all possible palindrome partitioning of s.
- * <p/>
+ * <p>
  * For example, given s = "aab",
  * Return
+ * <p>
+ * [
+ * ["aa","b"],
+ * ["a","a","b"]
+ * ]
  */
 public class Q131 {
 
-	public List<List<String>> partition(String s) {
-		List<List<String>> res = new ArrayList<>();
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
 
-		if (s.length() < 2) {
-			List<String> self = new ArrayList<>();
-			if (!s.isEmpty()) {
-				self.add(s);
-			}
-			res.add(self);
-			return res;
-		}
+        if (s.length() < 2) {
+            List<String> self = new ArrayList<>();
+            if (!s.isEmpty()) {
+                self.add(s);
+            }
+            res.add(self);
+            return res;
+        }
 
-		for (int i = s.length() - 1; i >= 0; i--) {
-			String second = s.substring(i);
-			if (isPalindrome(second)) {
-				List<List<String>> firsts = partition(s.substring(0, i));
-				for (List<String> first : firsts) {
-					first.add(second);
-					res.add(first);
-				}
-			}
-		}
+        for (int i = s.length() - 1; i >= 0; i--) {
+            String second = s.substring(i);
+            if (isPalindrome(second)) {
+                List<List<String>> firsts = partition(s.substring(0, i));
+                for (List<String> first : firsts) {
+                    first.add(second);
+                    res.add(first);
+                }
+            }
+        }
 
-		return res;
-	}
+        return res;
+    }
 
-	private boolean isPalindrome(String s) {
-		int i = 0, j = s.length() - 1;
-		while (s.charAt(i) == s.charAt(j) && i < j) {
-			i++;
-			j--;
-		}
-		return i >= j;
-	}
+    private boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (s.charAt(i) == s.charAt(j) && i < j) {
+            i++;
+            j--;
+        }
+        return i >= j;
+    }
 
 
+    public List<List<String>> partition2(String s) {
 
-	public List<List<String>> partition2(String s) {
+        List<List<Integer>> info = new ArrayList<>();
 
-		List<List<Integer>> info = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            info.add(new ArrayList<>());
 
-		for (int i = 0; i < s.length(); i++) {
-			info.add(new ArrayList<>());
+            for (int j = i; j < s.length(); j++) {
+                if (isPalindrome(s, i, j)) {
+                    info.get(i).add(j);
+                }
+            }
+        }
 
-			for (int j = i; j < s.length(); j++) {
-				if (isPalindrome(s, i, j)) {
-					info.get(i).add(j);
-				}
-			}
-		}
+        return process(info, 0, s);
+    }
 
-		return process(info, 0, s);
-	}
+    private List<List<String>> process(List<List<Integer>> info, int i, String s) {
+        List<List<String>> res = new ArrayList<>();
 
-	private List<List<String>> process(List<List<Integer>> info, int i, String s) {
-		List<List<String>> res = new ArrayList<>();
+        if (i >= s.length()) {
+            res.add(new ArrayList<>());
+            return res;
+        }
 
-		if (i >= s.length()) {
-			res.add(new ArrayList<>());
-			return res;
-		}
+        List<Integer> js = info.get(i);
+        for (Integer j : js) {
+            List<List<String>> nexts = process(info, j + 1, s);
+            for (List<String> next : nexts) {
+                next.add(0, s.substring(i, j + 1));
+                res.add(next);
+            }
+        }
 
-		List<Integer> js = info.get(i);
-		for (Integer j : js) {
-			List<List<String>> nexts = process(info, j + 1, s);
-			for (List<String> next : nexts) {
-				next.add(0, s.substring(i, j + 1));
-				res.add(next);
-			}
-		}
+        return res;
+    }
 
-		return res;
-	}
-
-	private boolean isPalindrome(String s, int i, int j) {
-		while (i < j) {
-			if (s.charAt(i) != s.charAt(j)) {
-				return false;
-			}
-			i++;
-			j--;
-		}
-		return true;
-	}
+    private boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 
 }
