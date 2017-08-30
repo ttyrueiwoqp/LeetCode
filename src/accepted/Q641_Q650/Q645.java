857c7951af264a954cf764b7768f300d45b782ca
Lu Fangjian <lufangjiannus@gmail.com>
2017-08-30 15:14:08 +0800

    Completed Q645

--- src/accepted/Q641_Q650/Q645.java
+++ src/accepted/Q641_Q650/Q645.java
@@ -1,9 +1,11 @@
 package accepted.Q641_Q650;
 
 /**
- * The set S originally contains numbers from 1 to n. But unfortunately, due to the data error, one of the numbers in the set got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+ * The set S originally contains numbers from 1 to n. But unfortunately, due to the data error, one of the numbers in
+ * the set got duplicated to another number in the set, which results in repetition of one number and loss of another number.
  * <p>
- * Given an array nums representing the data status of this set after the error. Your task is to firstly find the number occurs twice and then find the number that is missing. Return them in the form of an array.
+ * Given an array nums representing the data status of this set after the error. Your task is to firstly find the number
+ * occurs twice and then find the number that is missing. Return them in the form of an array.
  * <p>
  * Example 1:
  * Input: nums = [1,2,2,4]
