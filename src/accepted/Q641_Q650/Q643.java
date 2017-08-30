0d78b4eae18acbc9d80c6a651383afd82cb1a476
Lu Fangjian <lufangjiannus@gmail.com>
2017-08-30 15:31:32 +0800

    Completed Q643

---
+++ src/accepted/Q641_Q650/Q643.java
@@ -0,0 +1,30 @@
+package accepted.Q641_Q650;
+
+/**
+ * Given an array consisting of n integers, find the contiguous subarray of given length k that has the maximum average
+ * value. And you need to output the maximum average value.
+ * <p>
+ * Example 1:
+ * Input: [1,12,-5,-6,50,3], k = 4
+ * Output: 12.75
+ * Explanation: Maximum average is (12-5-6+50)/4 = 51/4 = 12.75
+ * Note:
+ * 1 <= k <= n <= 30,000.
+ * Elements of the given array will be in the range [-10,000, 10,000].
+ */
+public class Q643 {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        int max = sum;
+        for (int i = k; i < nums.length; i++) {
+            sum = sum - nums[i - k] + nums[i];
+            max = Math.max(sum, max);
+        }
+
+        return 1.0 * max / k;
+    }
+}
